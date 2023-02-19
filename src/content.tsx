00c0184b6e0267f7a092af3aa92dfa2822e42980
shunsuke-yamamoto <78201056+ys-eng@users.noreply.github.com>
2023-02-20 04:11:35 +0900

    feat: visible when selected text

--- src/content.tsx
+++ src/content.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react"
+import { useEffectOnce } from "react-use"
+
 import { GoogleButton } from "~components/GoogleButton"
 import { getGoogleSearchURL } from "~utils/getGoogleSearchURL"
 
 const Content = () => {
+  const [selectedText, setSelectedText] = useState<string>("")
+
   const onClickSearchButton = () => {
-    const url = getGoogleSearchURL("test")
+    const url = getGoogleSearchURL(selectedText)
     window.open(url, "_blank", "noreferrer")
+  }
+
+  useEffectOnce(() => {
+    document.addEventListener("mouseup", () => {
+      // mouseup時には、選択された状態になっているので、処理を遅らせる
+      setTimeout(() => {
+        const text = document.getSelection().toString()
+        setSelectedText(text)
+      })
+    })
+  })
+
+  if (!selectedText) {
+    return null
   }
 
   return (
