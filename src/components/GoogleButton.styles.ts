58b0513036a5a94958bb169d10289b57e64fa8bc
shunsuke-yamamoto <78201056+ys-eng@users.noreply.github.com>
2023-02-26 14:41:56 +0900

    chore: add stylelint

--- src/components/GoogleButton.styles.ts
+++ src/components/GoogleButton.styles.ts
@@ -1,14 +1,14 @@
 import styled from "@emotion/styled";
 
 export const Root = styled.div`
+  display: flex;
   width: 30px;
   height: 30px;
-  background-color: white;
-  box-shadow: 0px 2px 16px rgb(0 0 0 / 16%);
-  border-radius: 50%;
-  user-select: none;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: white;
+  box-shadow: 0 2px 16px rgb(0 0 0 / 16%);
+  cursor: pointer;
+  user-select: none;
 `;
